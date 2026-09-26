d7566ba1965325c062690f0a838f4cc515519743
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-26 10:27:09 +0100

    Squashing Commit!

---
+++ src/ci3/pagination_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['pagination_first_link'] = '&lsaquo; 第一頁';
+$lang['pagination_last_link'] = '最後一頁 &rsaquo;';
+$lang['pagination_next_link'] = '&gt;';
+$lang['pagination_prev_link'] = '&lt;';
+$lang['pagination_summary_text'] = '顯示第 %d 至第 %d 筆，共 %d 筆記錄';
---
+++ src/info.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * CiSkeleton Language Metadata - Chinese (Traditional)
+ *
+ * @package     CiSkeleton\Core
+ * @subpackage  Language
+ * @author      Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright   Copyright (c) 2018-present, Kader Bouyakoub
+ */
+
+return [
+	'name'       => '繁體中文',
+	'name_en'    => 'Chinese (Traditional)',
+	'locale'     => 'zh_TW',
+	'tag'        => 'zh-TW',
+	'direction'  => 'ltr',
+	'code'       => 'zh-tw',
+	'flag'       => 'tw',
+	'version'    => '1.0.0',
+	'version_id' => 10000,
+
+	// Localized language names.
+	'localized_names' => [
+		'arabic'     => '阿拉伯語',
+		'armenian'   => '亞美尼亞語',
+		'bengali'    => '孟加拉語',
+		'brazilian'  => '葡萄牙語（巴西）',
+		'bulgarian'  => '保加利亞語',
+		'catalan'    => '加泰羅尼亞語',
+		'chinese'    => '中文（簡體）',
+		'croatian'   => '克羅地亞語',
+		'czech'      => '捷克語',
+		'danish'     => '丹麥語',
+		'dutch'      => '荷蘭語',
+		'english'    => '英語',
+		'estonian'   => '愛沙尼亞語',
+		'filipino'   => '菲律賓語',
+		'finnish'    => '芬蘭語',
+		'french'     => '法語',
+		'georgian'   => '喬治亞語',
+		'german'     => '德語',
+		'greek'      => '希臘語',
+		'hindi'      => '印地語',
+		'hungarian'  => '匈牙利語',
+		'indonesian' => '印度尼西亞語',
+		'italian'    => '意大利語',
+		'japanese'   => '日語',
+		'korean'     => '韓語',
+		'latvian'    => '拉脫維亞語',
+		'lithuanian' => '立陶宛語',
+		'malay'      => '馬來語',
+		'norwegian'  => '挪威語',
+		'persian'    => '波斯語',
+		'polish'     => '波蘭語',
+		'portuguese' => '葡萄牙語',
+		'romanian'   => '羅馬尼亞語',
+		'russian'    => '俄語',
+		'serbian'    => '塞爾維亞語',
+		'slovak'     => '斯洛伐克語',
+		'slovenian'  => '斯洛文尼亞語',
+		'spanish'    => '西班牙語',
+		'swahili'    => '斯瓦希里語',
+		'swedish'    => '瑞典語',
+		'taiwanese'  => '中文（繁體）',
+		'tamil'      => '泰米爾語',
+		'thai'       => '泰語',
+		'turkish'    => '土耳其語',
+		'ukrainian'  => '烏克蘭語',
+		'urdu'       => '烏爾都語',
+		'vietnamese' => '越南語',
+	],
+];
